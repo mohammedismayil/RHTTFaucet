32313c7dde74ec2655e9d771617b5d02380b74ac
Mohammed Ismayil <mohammedismayil10@gmail.com>
2022-04-17 12:01:39 +0530

    added tailwind in front end

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 
+import "./App.css";
 function App() {
   const [address, setAddress] = useState();
   const [transactionHash, setTXHash] = useState();
@@ -37,18 +38,26 @@ function App() {
 
   // }
   return (
-    <div className="App">
-      <h2>Testnet faucet</h2>
-
-      <form onSubmit={handleSubmit}>
-        <input
-          placeholder="Enter your address here"
-          onChange={(e) => setAddress(e.target.value)}
-        ></input>
-        <input type="submit" value="Submit" />
-      </form>
-
-      <h3>Transaction Hash: {transactionHash}</h3>
+    <div className=" flex-col flex  m-auto items-center bg-slate-200 h-screen">
+      <div className="bg-white p-10 rounded font-bold m-10 items-center ">
+        <h2 className="font-bold text-3xl">RHTT(ERC20) Testnet faucet</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            className="shadow appearance-none border-2 border-indigo-500/100 rounded w-full py-2  my-5 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="username"
+            type="text"
+            placeholder="Enter your address here"
+            onChange={(e) => setAddress(e.target.value)}
+          ></input>
+          <button
+            type="submit"
+            className=" w-full flex justify-center py-2   border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Send RHTT
+          </button>
+        </form>
+        {transactionHash ? <h3>Transaction Hash: {transactionHash}</h3> : null}
+      </div>
     </div>
   );
 }
