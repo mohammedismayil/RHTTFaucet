a7e4156306af68f78d761d32ecb316c0316c8023
Mohammed Ismayil <mohammedismayil10@gmail.com>
2022-04-20 09:39:21 +0530

    mongodb added for storing addressed

--- backend/server.js
+++ backend/server.js
@@ -1,343 +1,32 @@
 const express = require("express");
 require("dotenv").config();
+const mongoose = require("mongoose");
 const app = express();
 const port = 5000;
+main().catch((err) => console.log(err));
 const Web3 = require("web3");
-const web3 = new Web3(new Web3.providers.HttpProvider(process.env['Ropsten_url']));
-const privateKey = process.env['Private_key'];
+const { contractABI } = require("./contract");
+
+// let Mongodb_url = ;
+async function main() {
+  await mongoose.connect(
+    "mongodb+srv://ismayil:ismayilmi@cluster0.maogj.mongodb.net/rhhtfaucet?retryWrites=true&w=majority"
+  );
+}
+const kittySchema = new mongoose.Schema({
+  address: String,
+});
+const Kitten = mongoose.model("addresses", kittySchema);
+
+const web3 = new Web3(
+  new Web3.providers.HttpProvider(process.env["Ropsten_url"])
+);
+const privateKey = process.env["Private_key"];
 
 //Your Private key environment variable
 
-
-// const web3 = new Web3(new Web3.providers.HttpProvider(process.env.Ropsten_url));
-// const privateKey = process.env.Private_key; 
 let tokenAddress = "0x907cCf3732AcA4ED271A9b71F679901AbfB8c1C9"; // Demo Token contract address
-
-let toAddress = "0xc9e1bD5A93cf631716D3e71e20Ac9A3b2DF60008"; // where to send it
-
 let fromAddress = "0xd565a55b83fd384894f1165a69b649fd9372fc65"; // your wallet
-
-let contractABI = [
-  {
-    inputs: [],
-    stateMutability: "nonpayable",
-    type: "constructor",
-  },
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: true,
-        internalType: "address",
-        name: "owner",
-        type: "address",
-      },
-      {
-        indexed: true,
-        internalType: "address",
-        name: "spender",
-        type: "address",
-      },
-      {
-        indexed: false,
-        internalType: "uint256",
-        name: "value",
-        type: "uint256",
-      },
-    ],
-    name: "Approval",
-    type: "event",
-  },
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: true,
-        internalType: "address",
-        name: "from",
-        type: "address",
-      },
-      {
-        indexed: true,
-        internalType: "address",
-        name: "to",
-        type: "address",
-      },
-      {
-        indexed: false,
-        internalType: "uint256",
-        name: "value",
-        type: "uint256",
-      },
-    ],
-    name: "Transfer",
-    type: "event",
-  },
-  {
-    inputs: [],
-    name: "admin",
-    outputs: [
-      {
-        internalType: "address",
-        name: "",
-        type: "address",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "address",
-        name: "owner",
-        type: "address",
-      },
-      {
-        internalType: "address",
-        name: "spender",
-        type: "address",
-      },
-    ],
-    name: "allowance",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "address",
-        name: "spender",
-        type: "address",
-      },
-      {
-        internalType: "uint256",
-        name: "amount",
-        type: "uint256",
-      },
-    ],
-    name: "approve",
-    outputs: [
-      {
-        internalType: "bool",
-        name: "",
-        type: "bool",
-      },
-    ],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "address",
-        name: "account",
-        type: "address",
-      },
-    ],
-    name: "balanceOf",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "uint256",
-        name: "amount",
-        type: "uint256",
-      },
-    ],
-    name: "burn",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "decimals",
-    outputs: [
-      {
-        internalType: "uint8",
-        name: "",
-        type: "uint8",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "address",
-        name: "spender",
-        type: "address",
-      },
-      {
-        internalType: "uint256",
-        name: "subtractedValue",
-        type: "uint256",
-      },
-    ],
-    name: "decreaseAllowance",
-    outputs: [
-      {
-        internalType: "bool",
-        name: "",
-        type: "bool",
-      },
-    ],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "address",
-        name: "spender",
-        type: "address",
-      },
-      {
-        internalType: "uint256",
-        name: "addedValue",
-        type: "uint256",
-      },
-    ],
-    name: "increaseAllowance",
-    outputs: [
-      {
-        internalType: "bool",
-        name: "",
-        type: "bool",
-      },
-    ],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "address",
-        name: "to",
-        type: "address",
-      },
-      {
-        internalType: "uint256",
-        name: "amount",
-        type: "uint256",
-      },
-    ],
-    name: "mint",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "name",
-    outputs: [
-      {
-        internalType: "string",
-        name: "",
-        type: "string",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "symbol",
-    outputs: [
-      {
-        internalType: "string",
-        name: "",
-        type: "string",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "totalSupply",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "address",
-        name: "to",
-        type: "address",
-      },
-      {
-        internalType: "uint256",
-        name: "amount",
-        type: "uint256",
-      },
-    ],
-    name: "transfer",
-    outputs: [
-      {
-        internalType: "bool",
-        name: "",
-        type: "bool",
-      },
-    ],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "address",
-        name: "from",
-        type: "address",
-      },
-      {
-        internalType: "address",
-        name: "to",
-        type: "address",
-      },
-      {
-        internalType: "uint256",
-        name: "amount",
-        type: "uint256",
-      },
-    ],
-    name: "transferFrom",
-    outputs: [
-      {
-        internalType: "bool",
-        name: "",
-        type: "bool",
-      },
-    ],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-];
 
 let contract = new web3.eth.Contract(contractABI, tokenAddress, {
   from: fromAddress,
@@ -355,7 +44,6 @@ app.use(function (req, res, next) {
 });
 app.use(express.urlencoded({ extended: true }));
 app.post("/receivetoken", (req, res) => {
-  console.log(req.body);
   let data = contract.methods.transfer(req.body.toAddress, amount).encodeABI();
   let txObj = {
     gas: web3.utils.toHex(100000),
@@ -369,33 +57,58 @@ app.post("/receivetoken", (req, res) => {
     from: fromAddress,
   };
 
-  web3.eth.accounts.signTransaction(txObj, privateKey, (err, signedTx) => {
-    if (err) {
-      // return callback(err);
-    } else {
-      console.log(signedTx);
-
-      return web3.eth.sendSignedTransaction(
-        signedTx.rawTransaction,
-        (err, respo) => {
-          if (err) {
-            console.log(err);
-            // res.json({ message: err });
-            res.json({
-              message:
-                "looks like sometheing has happened.Mostly it should be the pending transaction",
-            });
-          } else {
-            console.log(respo);
-            res.json({ transactionHash: respo });
-          }
-        }
-      );
-    }
-  });
+  searchAddress(req.body.toAddress, res, txObj);
 });
 // ADD THIS LINE
-app.use(express.static('../build'));
+app.use(express.static("../build"));
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
+
+async function searchAddress(address, res, txObj) {
+  const kittens = await Kitten.find({
+    address: address,
+  });
+
+  console.log("given address is " + address);
+  console.log("txobj is " + txObj);
+  if (kittens.length > 0) {
+    console.log("i am here");
+
+    res.json({
+      message: "Address has been already present",
+    });
+  } else {
+    web3.eth.accounts.signTransaction(txObj, privateKey, (err, signedTx) => {
+      if (err) {
+        // return callback(err);
+        console.log(err);
+        res.json({
+          message: "sign transaction not completed",
+        });
+      } else {
+        console.log(signedTx);
+
+        return web3.eth.sendSignedTransaction(
+          signedTx.rawTransaction,
+          (err, respo) => {
+            if (err) {
+              console.log(err);
+              // res.json({ message: err });
+              res.json({
+                message:
+                  "looks like sometheing has happened.Mostly it should be the pending transaction",
+              });
+            } else {
+              console.log(respo);
+              const kittens = new Kitten({
+                address: address,
+              }).save();
+              res.json({ transactionHash: respo });
+            }
+          }
+        );
+      }
+    });
+  }
+}
